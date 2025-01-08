39a0efeed2db43a46bd2c936965a67cbfa765ea4
Azie <azilriego@gmail.com>
2025-01-08 08:07:08 +0800

    Add files via upload

---
+++ Final-Project/Final-Project.py
@@ -0,0 +1,86 @@
+import os
+import time
+
+os.system('cls')
+time.sleep(1)
+
+print("\t\t\t\t\t\t\tLoading.")
+print("\t\t\t\t\t\t\t  1%")
+print("\t\t\t\t\t\t_")
+time.sleep(1)
+
+os.system('cls')
+print("\t\t\t\t\t\t\tLoading..")
+print("\t\t\t\t\t\t\t  10%")
+print("\t\t\t\t\t\t___")
+time.sleep(1)
+
+os.system('cls')
+print("\t\t\t\t\t\t\tLoading...")
+print("\t\t\t\t\t\t\t  20%")
+print("\t\t\t\t\t\t______")
+time.sleep(1)
+
+os.system('cls')
+print("\t\t\t\t\t\t\tLoading.")
+print("\t\t\t\t\t\t\t  40%")
+print("\t\t\t\t\t\t_________")
+time.sleep(1)
+
+os.system('cls')
+print("\t\t\t\t\t\t\tLoading..")
+print("\t\t\t\t\t\t\t  50%")
+print("\t\t\t\t\t\t____________")
+time.sleep(1)
+
+os.system('cls')
+print("\t\t\t\t\t\t\tLoading...")
+print("\t\t\t\t\t\t\t  60%")
+print("\t\t\t\t\t\t______________")
+time.sleep(1)
+
+os.system('cls')
+print("\t\t\t\t\t\t\tLoading.")
+print("\t\t\t\t\t\t\t  70%")
+print("\t\t\t\t\t\t_________________")
+time.sleep(1)
+
+os.system('cls')
+print("\t\t\t\t\t\t\tLoading..")
+print("\t\t\t\t\t\t\t  80%")
+print("\t\t\t\t\t\t____________________")
+time.sleep(1)
+
+os.system('cls')
+print("\t\t\t\t\t\t\tLoading...")
+print("\t\t\t\t\t\t\t  90%")
+print("\t\t\t\t\t\t______________________")
+time.sleep(1)
+
+os.system('cls')
+print("\t\t\t\t\t\t\tComplete!")
+print("\t\t\t\t\t\t\t  100%")
+print("\t\t\t\t\t\t____________________________")
+time.sleep(5)
+
+os.system('cls')
+print("\t\t\t\t\t\t\tWelcome!")
+time.sleep(1)
+
+os.system('cls')
+print("\t\t\t\t\t\t\tWelcome!")
+print("\n\t\t\t\t\t    This Program Is Brought To You By:")
+time.sleep(1)
+
+os.system('cls')
+print("\t\t\t\t\t\t\tWelcome!")
+print("\n\t\t\t\t\t    This Program Was Brought To You By:")
+print("\n\t\t\t\t\t\t   Azil Nheecolle Y. Riego")
+time.sleep(1)
+
+os.system('cls')
+print("\t\t\t\t\t\t\tWelcome!")
+print("\n\t\t\t\t\t    This Program Was Brought To You By:")
+print("\n\t\t\t\t\t\t   Azil Nheecolle Y. Riego")
+print("\n\t\t\t\t\t\t\tFrom BSIT-1A")
+time.sleep(1)
