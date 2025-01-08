e63232e48d27f9f9c21eb955440fb9d212063ed9
Azie <azilriego@gmail.com>
2025-01-08 11:49:29 +0800

    Create Activity8.py

---
+++ Activities/Activity8.py
@@ -0,0 +1,8 @@
+password = input("Please enter a password: ")
+	if password.lower() == "password":
+		print("Access Granted!")
+		print("Enjoy the program.")
+	else:
+		print("Acess Denied")
+
+	print("Thank you for using the program.")
